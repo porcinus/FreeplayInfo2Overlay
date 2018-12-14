b54a2c33327e22809aa2db92d6fab0339f7d867f
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-14 19:02:57 +0100

    Update example-overlay.sh

--- example-overlay.sh
+++ example-overlay.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # to run aside of example-framebuffer.sh
-nns-overlay-deamon -pin 41 -srtfile "/dev/shm/vbat.srt" -screenwidth 1024 -height 50
+./nns-overlay-deamon -pin 41 -srtfile "/dev/shm/vbat.srt" -screenwidth 1024 -height 50
