cb80c960b95767e9554a7e036e06bc89f8ab63d3
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-14 18:57:48 +0100

    Update example-framebuffer.sh

--- example-framebuffer.sh
+++ example-framebuffer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 while [ TRUE ]; do
-info2png -i2cbus "/dev/i2c-1" -i2caddress 0x4d -adcvref 3.25 -adcres 4096 -r1value 91 -r2value 220 -vbatlow 3.5 -width 304 -height 10 -o "/dev/shm"
+info2png -i2cbus "/dev/i2c-1" -i2caddress 0x4d -adcvref 3.28 -adcres 4096 -r1value 9.1 -r2value 21.3 -vbatlow 3.5 -vbatlogging -width 304 -height 11 -o "/dev/shm"
 png2fb16 -i "/dev/shm/fb_footer.png" -f "/dev/fb1" -xoffset 16 -yoffset 214
 sleep 15
 done
