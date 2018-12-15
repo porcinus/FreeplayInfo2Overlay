8556d8a3cd9d8c528def8952bf00547e4fa222fc
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-15 17:33:04 +0100

    Update info2overlay.sh

--- service/info2overlay.sh
+++ service/info2overlay.sh
@@ -3,4 +3,4 @@ killall info2png
 killall png2fb16 
 killall nns-overlay-deamon 
 /home/pi/NNS/FreeplayInfo2Overlay/info2png -i2cbus "/dev/i2c-1" -i2caddress 0x4d -adcvref 3.28 -adcres 4096 -r1value 9.1 -r2value 21.3 -vbatlow 3.5 -vbatlogging -o "/dev/shm" -interval 15 & \
-/home/pi/NNS/FreeplayInfo2Overlay./nns-overlay-deamon -pin 41 -srtfile "/dev/shm/vbat.srt" -screenwidth 1024 -height 50 -interval 250
+/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -srtfile "/dev/shm/vbat.srt" -screenwidth 1024 -height 50 -interval 250
