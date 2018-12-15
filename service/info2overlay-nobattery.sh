4348675d35f69ccf8aa7db0d69acdc66830f4658
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-15 17:32:50 +0100

    Update info2overlay-nobattery.sh
    update path

--- service/info2overlay-nobattery.sh
+++ service/info2overlay-nobattery.sh
@@ -2,5 +2,5 @@
 killall info2png
 killall png2fb16 
 killall nns-overlay-deamon 
-./info2png -o "/dev/shm" -interval 15 & \ 
-./nns-overlay-deamon -pin 41 -srtfile "/dev/shm/vbat.srt" -screenwidth 1024 -height 50 -interval 250
+/home/pi/NNS/FreeplayInfo2Overlay/info2png -o "/dev/shm" -interval 15 & \ 
+/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -srtfile "/dev/shm/vbat.srt" -screenwidth 1024 -height 50 -interval 250
