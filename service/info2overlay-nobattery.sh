82a12f0b140431754d28d72418a316f71eed96db
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-16 02:05:54 +0100

    Add files via upload

--- service/info2overlay-nobattery.sh
+++ service/info2overlay-nobattery.sh
@@ -2,5 +2,5 @@
 killall info2png
 killall png2fb16 
 killall nns-overlay-deamon 
-/home/pi/NNS/FreeplayInfo2Overlay/info2png -o "/dev/shm" -width 304 -height 11 -video 5 -interval 15 & \ 
-/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.avi" -screenwidth 1024 -height 50 -interval 250
+/home/pi/NNS/FreeplayInfo2Overlay/info2png -o "/dev/shm" -width 304 -height 11 -interval 15 & \ 
+/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -screenwidth 1024 -height 50 -interval 250
