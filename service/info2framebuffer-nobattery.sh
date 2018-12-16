04fd5894297a7300b94d6eef9edae91e991bb098
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-16 16:41:31 +0100

    Add files via upload

--- service/info2framebuffer-nobattery.sh
+++ service/info2framebuffer-nobattery.sh
@@ -2,5 +2,5 @@
 killall info2png
 killall png2fb16
 killall nns-overlay-deamon 
-/home/pi/NNS/FreeplayInfo2Overlay/info2png -width 304 -height 11 -o "/dev/shm" -interval 15 & \ 
+/home/pi/NNS/FreeplayInfo2Overlay/info2png -width 300 -height 11 -o "/dev/shm" -interval 15 & \ 
 /home/pi/NNS/FreeplayInfo2Overlay/png2fb16 -i "/dev/shm/fb_footer.png" -f "/dev/fb1" -xoffset 16 -yoffset 214 -interval 15 
