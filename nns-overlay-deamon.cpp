b386a19c855dd82c8ab288a1ad5c014326666a46
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-16 02:06:20 +0100

    Add files via upload

--- nns-overlay-deamon.cpp
+++ nns-overlay-deamon.cpp
@@ -23,21 +23,24 @@ bool gpio_activelow=false;						//gpio active low
 char gpio_buffer[4];									//gpio read buffer
 int gpio_interval=-1;									//gpio check interval
 int gpio_value;												//gpio value
-char *avi_path;												//full path to str file
-bool avi_exist=false;									//file exist?
-char program_path[PATH_MAX];					//full path to this program
+char *png_path;												//full path to str file
+bool png_exist=false;									//file exist?
+//char program_path[PATH_MAX];					//full path to this program
 int screen_width=-1;									//screen width
 int bar_height=-1;									  //bar height
+int duration = -1;							//video duration
 char omx_exec_path[PATH_MAX];					//full command line to run omx
+char ffmpeg_exec_path[PATH_MAX];	//full command line to run ffmpeg
 
 
 
 void show_usage(void){
-	printf("Example : ./nns-overlay-deamon -pin 41 -interval 200 -file \"/dev/shm/fb_footer.avi\" -screenwidth 1024 -height 40\n");
+	printf("Example : ./nns-overlay-deamon -pin 41 -interval 200 -file \"/dev/shm/fb_footer.png\" -duration 5 -screenwidth 1024 -height 40\n");
 	printf("Options:\n");
 	printf("\t-pin, pin number corresponding to input to monitor\n");
 	printf("\t-interval, optional, pin checking interval in msec\n");
-	printf("\t-file, full path to avi file, used for OSD\n");
+	printf("\t-file, full path to png file, used for OSD\n");
+	printf("\t-duration, in sec, used for OSD\n");
 	printf("\t-screenwidth, screen width, used for OSD\n");
 	printf("\t-height, bar height, used for OSD\n");
 }
@@ -45,6 +48,7 @@ void show_usage(void){
 int main(int argc, char *argv[]){
 	if(argc<9){show_usage();return 1;} //wrong arguments count
 	if(access("/usr/bin/omxplayer",F_OK)!=0){printf("Failed, require OMXplayer\n");return 1;} //'omxplayer' is not installed
+	if(access("/usr/bin/ffmpeg",F_OK)!=0){printf("Failed, require ffmpeg\n");return 1;} //'ffmpeg' is not installed
 	
 	sleep(2);
 	
@@ -52,22 +56,23 @@ int main(int argc, char *argv[]){
 		if(strcmp(argv[i],"-help")==0){show_usage();return 1;
 		}else if(strcmp(argv[i],"-pin")==0){gpio_pin=atoi(argv[i+1]);snprintf(gpio_path,sizeof(gpio_path),"/sys/class/gpio/gpio%i/",gpio_pin);
 		}else if(strcmp(argv[i],"-interval")==0){gpio_interval=atoi(argv[i+1]);
-		}else if(strcmp(argv[i],"-file")==0){avi_path=(char*)argv[i+1];
+		}else if(strcmp(argv[i],"-file")==0){png_path=(char*)argv[i+1];
+		}else if(strcmp(argv[i],"-duration")==0){duration=atoi(argv[i+1]);
 		}else if(strcmp(argv[i],"-screenwidth")==0){screen_width=atoi(argv[i+1]);
 		}else if(strcmp(argv[i],"-height")==0){bar_height=atoi(argv[i+1]);}
 	}
 	
-	if(gpio_pin<0||screen_width<0||bar_height<0){printf("Failed, missing pin argument\n");show_usage();return 1;} //user miss some needed arguments
+	if(gpio_pin<0||screen_width<0||bar_height<0||duration<0){printf("Failed, missing some arguments\n");show_usage();return 1;} //user miss some needed arguments
 	if(gpio_interval<100||gpio_interval>600){printf("Warning, wrong cheking interval set, setting it to 200msec\n");gpio_interval=200;} //wrong interval
 	
 	
-	while(!avi_exist){
-		if(access(avi_path,R_OK)!=0){
-			printf("Failed, %s not readable, retrying\n",avi_path);
+	while(!png_exist){
+		if(access(png_path,R_OK)!=0){
+			printf("Failed, %s not readable, retrying\n",png_path);
 			sleep(5);
 		}else{
-			printf("%s found\n",avi_path);
-			avi_exist=true;
+			printf("%s found\n",png_path);
+			png_exist=true;
 		}
 	}
 	
@@ -85,7 +90,7 @@ int main(int argc, char *argv[]){
 	
 
 
-strncpy(program_path,argv[0],strlen(argv[0])-19); //backup program path
+//strncpy(program_path,argv[0],strlen(argv[0])-19); //backup program path
 chdir(gpio_path); //change directory to gpio sysfs
 	
 	
@@ -105,8 +110,10 @@ chdir(gpio_path); //change directory to gpio sysfs
 	if(strcmp(gpio_buffer,"1")==0){gpio_activelow=true;} //parse gpio active low
 	//printf("GPIO: active_low is %s\n",gpio_buffer);
 	
-	//snprintf(omx_exec_path,sizeof(omx_exec_path),"omxplayer --no-osd --no-keys --alpha 150 --layer 2000 --win 0,0,%i,%i --align center --font-size 750 --no-ghost-box --subtitles \"%s\" \"%s/black.avi\" >/dev/null 2>&1",screen_width,bar_height,avi_path,program_path); //parse command line for omx
-	snprintf(omx_exec_path,sizeof(omx_exec_path),"omxplayer --no-osd --no-keys --layer 2000 --win 0,0,%i,%i \"%s\" >/dev/null 2>&1",screen_width,bar_height,avi_path); //parse command line for omx
+	//snprintf(omx_exec_path,sizeof(omx_exec_path),"omxplayer --no-osd --no-keys --alpha 150 --layer 2000 --win 0,0,%i,%i --align center --font-size 750 --no-ghost-box --subtitles \"%s\" \"%s/black.avi\" >/dev/null 2>&1",screen_width,bar_height,png_path,program_path); //parse command line for omx
+	
+	snprintf(ffmpeg_exec_path,sizeof(ffmpeg_exec_path),"nice -5 ffmpeg -loglevel panic -y -loop 1 -i fb_footer.png -t %i -r 5  -force_key_frames 1 -c:v mjpeg -f avi fb_footer.avi >/dev/null 2>&1",duration); //parse command line for ffmpeg
+	snprintf(omx_exec_path,sizeof(omx_exec_path),"omxplayer --no-osd --no-keys --layer 2000 --win 0,0,%i,%i \"%s.avi\" >/dev/null 2>&1",screen_width,bar_height,png_path); //parse command line for omx
 		
 	while(true){
 		chdir(gpio_path); //change directory to gpio sysfs
@@ -114,6 +121,7 @@ chdir(gpio_path); //change directory to gpio sysfs
 		gpio_value=atoi(gpio_buffer); //parse gpio value
 		if((gpio_value==0&&!gpio_activelow)||(gpio_value==1&&gpio_activelow)){ //gpio button pressed
 			//chdir(program_path); //change directory
+			system(ffmpeg_exec_path); //convert png to avi
 			system(omx_exec_path); //run omxplayer
 		}
 		usleep(gpio_interval*1000); //sleep
