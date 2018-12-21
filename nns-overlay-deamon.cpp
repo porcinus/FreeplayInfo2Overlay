5a00fa1a04497730a0854c777898f005bb85a0b9
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-21 16:03:47 +0100

    Add files via upload
    Implement of img2dispmanx, no more need ffmpeg and omxplayer

--- nns-overlay-deamon.cpp
+++ nns-overlay-deamon.cpp
@@ -26,12 +26,13 @@ int gpio_interval=-1;									//gpio check interval
 int gpio_value;												//gpio value
 char *png_path;												//full path to str file
 bool png_exist=false;									//file exist?
-//char program_path[PATH_MAX];					//full path to this program
+char program_path[PATH_MAX];					//full path to this program
 int screen_width=-1;									//screen width
-int bar_height=-1;									  //bar height
+int bar_height=0;									  //bar height
 int duration = -1;							//video duration
-char omx_exec_path[PATH_MAX];					//full command line to run omx
-char ffmpeg_exec_path[PATH_MAX];	//full command line to run ffmpeg
+//char omx_exec_path[PATH_MAX];					//full command line to run omx
+//char ffmpeg_exec_path[PATH_MAX];	//full command line to run ffmpeg
+char img2dispmanx_exec_path[PATH_MAX];	//full command line to run img2dispmanx
 
 
 
@@ -44,13 +45,13 @@ void show_usage(void){
 	printf("\t-file, full path to png file, used for OSD\n");
 	printf("\t-duration, in sec, used for OSD\n");
 	printf("\t-screenwidth, screen width, used for OSD\n");
-	printf("\t-height, bar height, used for OSD\n");
+	printf("\t-height, bar height, optional, used for OSD\n");
 }
 
 int main(int argc, char *argv[]){
 	if(argc<9){show_usage();return 1;} //wrong arguments count
-	if(access("/usr/bin/omxplayer",F_OK)!=0){printf("Failed, require OMXplayer\n");return 1;} //'omxplayer' is not installed
-	if(access("/usr/bin/ffmpeg",F_OK)!=0){printf("Failed, require ffmpeg\n");return 1;} //'ffmpeg' is not installed
+	//if(access("/usr/bin/omxplayer",F_OK)!=0){printf("Failed, require OMXplayer\n");return 1;} //'omxplayer' is not installed
+	//if(access("/usr/bin/ffmpeg",F_OK)!=0){printf("Failed, require ffmpeg\n");return 1;} //'ffmpeg' is not installed
 	
 	sleep(2);
 	
@@ -65,7 +66,7 @@ int main(int argc, char *argv[]){
 		}else if(strcmp(argv[i],"-height")==0){bar_height=atoi(argv[i+1]);}
 	}
 	
-	if(gpio_pin<0||screen_width<0||bar_height<0||duration<0){printf("Failed, missing some arguments\n");show_usage();return 1;} //user miss some needed arguments
+	if(gpio_pin<0||screen_width<0||duration<0){printf("Failed, missing some arguments\n");show_usage();return 1;} //user miss some needed arguments
 	if(gpio_interval<100||gpio_interval>600){printf("Warning, wrong cheking interval set, setting it to 200msec\n");gpio_interval=200;} //wrong interval
 	
 	if(gpio_reverselogic){printf("Reversed activelow logic\n");}
@@ -91,11 +92,14 @@ int main(int argc, char *argv[]){
 	
 
 
-	//getcwd(program_path,sizeof(program_path)); //backup program path
+strncpy(program_path,argv[0],strlen(argv[0])-19); //backup program path
+//printf("program path:%s\n",program_path);
+if(strcmp(program_path,".")==0){
+	getcwd(program_path,sizeof(program_path)); //backup program path
+	//printf("program path:%s\n",program_path);
+}
 	
 
-
-//strncpy(program_path,argv[0],strlen(argv[0])-19); //backup program path
 chdir(gpio_path); //change directory to gpio sysfs
 	
 	
@@ -117,19 +121,22 @@ chdir(gpio_path); //change directory to gpio sysfs
 	
 	//snprintf(omx_exec_path,sizeof(omx_exec_path),"omxplayer --no-osd --no-keys --alpha 150 --layer 20000 --win 0,0,%i,%i --align center --font-size 750 --no-ghost-box --subtitles \"%s\" \"%s/black.avi\" >/dev/null 2>&1",screen_width,bar_height,png_path,program_path); //parse command line for omx
 	
-	snprintf(ffmpeg_exec_path,sizeof(ffmpeg_exec_path),"nice -5 ffmpeg -loglevel panic -y -loop 1 -i \"%s\" -t %i -r 5  -force_key_frames 1 -c:v mjpeg -f avi \"%s.avi\" >/dev/null 2>&1",png_path,duration,png_path); //parse command line for ffmpeg
-	snprintf(omx_exec_path,sizeof(omx_exec_path),"omxplayer -z --no-osd --no-keys --layer 20000 --win 0,0,%i,%i \"%s.avi\" >/dev/null 2>&1",screen_width,bar_height,png_path); //parse command line for omx
+	//snprintf(ffmpeg_exec_path,sizeof(ffmpeg_exec_path),"nice -5 ffmpeg -loglevel panic -y -loop 1 -i \"%s\" -t %i -r 5  -force_key_frames 1 -c:v mjpeg -f avi \"%s.avi\" >/dev/null 2>&1",png_path,duration,png_path); //parse command line for ffmpeg
+	//snprintf(omx_exec_path,sizeof(omx_exec_path),"omxplayer --no-osd --no-keys --layer 20000 --win 0,0,%i,%i \"%s.avi\" >/dev/null 2>&1",screen_width,bar_height,png_path); //parse command line for omx
 	
-
+	
+	snprintf(img2dispmanx_exec_path,sizeof(img2dispmanx_exec_path),"timeout %i %s/img2dispmanx -file \"%s\" -width %i -height %i -layer 20000  >/dev/null 2>&1",duration,program_path,png_path,screen_width,bar_height); //parse command line for img2dispmanx
 
 	while(true){
 		chdir(gpio_path); //change directory to gpio sysfs
 		temp_filehandle = fopen("value","r"); fgets(gpio_buffer,sizeof(gpio_buffer),temp_filehandle); fclose(temp_filehandle); //read gpio value
 		gpio_value=atoi(gpio_buffer); //parse gpio value
 		if((gpio_value==0&&(!gpio_activelow&&!gpio_reverselogic||gpio_activelow&&gpio_reverselogic))||(gpio_value==1&&(gpio_activelow&&!gpio_reverselogic||!gpio_activelow&&gpio_reverselogic))){ //gpio button pressed
-			//chdir(program_path); //change directory
-			system(ffmpeg_exec_path); //convert png to avi
-			system(omx_exec_path); //run omxplayer
+			chdir(program_path); //change directory
+			//system(ffmpeg_exec_path); //convert png to avi
+			//system(omx_exec_path); //run omxplayer
+			//printf("program path:%s\n",img2dispmanx_exec_path);
+			system(img2dispmanx_exec_path); //run omxplayer
 		}
 		usleep(gpio_interval*1000); //sleep
 	}
