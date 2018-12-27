540ec96e518cd03c865d530f4e258a5c78bff517
porcinus <37425709+porcinus@users.noreply.github.com>
2018-12-27 16:04:25 +0100

    Update info2overlay-nobattery.sh
    adding -lowbatpin 7

--- service/info2overlay-nobattery.sh
+++ service/info2overlay-nobattery.sh
@@ -3,4 +3,4 @@ killall info2png
 killall png2fb16 
 killall nns-overlay-deamon 
 /home/pi/NNS/FreeplayInfo2Overlay/info2png -o "/dev/shm" -width 300 -height 12 -interval 15 & \ 
-/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -screenwidth 1024 -height 45 -interval 250
+/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -screenwidth 1024 -height 45 -interval 250 -lowbatpin 7
