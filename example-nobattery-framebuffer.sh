af859fca20c5c2fb3ef46e0a85930da28740f38c
porcinus <37425709+porcinus@users.noreply.github.com>
2019-04-21 14:50:27 +0200

    Delete example-nobattery-framebuffer.sh
    remove the framebuffer part

--- example-nobattery-framebuffer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-killall info2png
-killall png2fb16
-killall nns-overlay-deamon 
-./info2png -width 300 -height 12 -o "/dev/shm" -interval 15 & \ 
-./png2fb16 -i "/dev/shm/fb_footer.png" -f "/dev/fb1" -xoffset 16 -yoffset 214 -interval 15 
