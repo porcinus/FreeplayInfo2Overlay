d1575662e8b24c517e5ce049f957314105537614
porcinus <37425709+porcinus@users.noreply.github.com>
2019-06-24 18:11:07 +0200

    Delete example-overlay.sh

--- example-overlay.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-killall info2png
-killall nns-overlay-deamon 
-./info2png -i2cbus "/dev/i2c-1" -i2caddress 0x4d -adcvref 3.28 -adcres 4096 -r1value 9.1 -r2value 21.3 -vbatlow 3.5 -vbatlogging -width 300 -height 12 -o "/dev/shm" -interval 10 & \
-./nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -interval 250
