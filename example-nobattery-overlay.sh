969311330ea5b053f2a65d04777c332129a85696
porcinus <37425709+porcinus@users.noreply.github.com>
2019-06-24 18:10:57 +0200

    Delete example-nobattery-overlay.sh

--- example-nobattery-overlay.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-killall info2png
-killall nns-overlay-deamon 
-./info2png -o "/dev/shm" -interval 15 -width 300 -height 12 & \ 
-./nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -interval 250
