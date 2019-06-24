6506ce770410c8965a321a7c150085323a988539
porcinus <37425709+porcinus@users.noreply.github.com>
2019-06-24 18:15:53 +0200

    Add files via upload

---
+++ service/info2overlay.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+killall info2png
+killall png2fb16 
+killall nns-overlay-deamon 
+/home/pi/NNS/FreeplayInfo2Overlay/info2png -pca9633adress 0x62 -i2cbus "/dev/i2c-1" -height 12 -o "/dev/shm" -interval 10 & \
+/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -interval 250 -lowbatpin 7
