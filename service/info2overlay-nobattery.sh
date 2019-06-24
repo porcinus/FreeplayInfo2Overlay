05ed765a553050d36ffd983c205eb595abe70ddd
porcinus <37425709+porcinus@users.noreply.github.com>
2019-06-24 18:12:56 +0200

    Delete info2overlay-nobattery.sh

--- service/info2overlay-nobattery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-killall info2png
-killall png2fb16 
-killall nns-overlay-deamon 
-/home/pi/NNS/FreeplayInfo2Overlay/info2png -o "/dev/shm" -width 300 -height 12 -interval 15 & \ 
-/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -interval 250
