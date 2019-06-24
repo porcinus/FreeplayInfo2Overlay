9d3d41c74a6710eba0c8de1235f6c817027d9dc4
porcinus <37425709+porcinus@users.noreply.github.com>
2019-06-24 18:13:00 +0200

    Delete info2overlay.sh

--- service/info2overlay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-killall info2png
-killall png2fb16 
-killall nns-overlay-deamon 
-/home/pi/NNS/FreeplayInfo2Overlay/info2png -i2cbus "/dev/i2c-1" -i2caddress 0x4d -adcvref 3.28 -adcres 4096 -r1value 9.1 -r2value 21.3 -vbatlow 3.5 -vbatlogging -width 300 -height 12 -o "/dev/shm" -interval 10 & \
-/home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -interval 250 -lowbatpin 7
