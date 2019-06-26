5e610ef97e540466b088983f97ac802ea9c4a9f3
porcinus <37425709+porcinus@users.noreply.github.com>
2019-06-26 19:35:12 +0200

    Update info2overlay.sh

--- service/info2overlay.sh
+++ service/info2overlay.sh
@@ -2,5 +2,5 @@
 killall info2png
 killall png2fb16 
 killall nns-overlay-deamon 
-/home/pi/NNS/FreeplayInfo2Overlay/info2png -i2cbus "/dev/i2c-1" -height 12 -o "/dev/shm" -interval 10 & \
+/home/pi/NNS/FreeplayInfo2Overlay/info2png -height 12 -o "/dev/shm" -interval 10 & \
 /home/pi/NNS/FreeplayInfo2Overlay/nns-overlay-deamon -pin 41 -file "/dev/shm/fb_footer.png" -duration 5 -interval 250 -lowbatpin 7
